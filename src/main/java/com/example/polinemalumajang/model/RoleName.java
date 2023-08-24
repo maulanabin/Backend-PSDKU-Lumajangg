60cbd6dc9a362054d13b5985f15858a9a003e8f4
maulanabint <bintang.sekawanmedia@gmail.com>
2023-08-24 08:18:53 +0700

    Commit for Spring Boot, Spring Security,JWT, MySQL

---
+++ src/main/java/com/example/polinemalumajang/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.example.polinemalumajang.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/test/java/com/example/polinemalumajang/PolinemaLumajangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polinemalumajang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolinemaLumajangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
